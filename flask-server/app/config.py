34480cada6a827f3d5c8b69e72bd56155438b60b
Tasihasi <taskovics.dani12@gmail.com>
2024-11-20 04:24:17 +0100

    created db

--- flask-server/app/config.py
+++ flask-server/app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    SECRET_KEY = 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DATABASE = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'  # SQLite database URI
+    DATABASE = os.path.join(os.getcwd(), 'app', 'flask_app', 'app.db')  # Absolute path to app.db
--- flask-server/app/flask_app/__init__.py
+++ flask-server/app/flask_app/__init__.py
@@ -6,7 +6,7 @@ from . import db
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app.config.from_object('app.config.Config')
 
     # Initialize the database with the app
     db.init_app(app)
